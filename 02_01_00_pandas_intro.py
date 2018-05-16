d26e391d9e74c62ca6dc5f9d756bc304cda37cc3
David Vaquero <pepesan@gmail.com>
2018-05-16 11:44:08 +0200

    carga de datos de iris_a un dataframe de pandas

--- 02_01_00_pandas_intro.py
+++ 02_01_00_pandas_intro.py
@@ -81,3 +81,24 @@ print("The most popular name")
 df=df[df['Births'] == df['Births'].max()]
 #plt.show()
 """
+
+
+import numpy as np
+from sklearn.datasets import load_iris
+
+# save load_iris() sklearn dataset to iris
+# if you'd like to check dataset type use: type(load_iris())
+# if you'd like to view list of attributes use: dir(load_iris())
+iris = load_iris()
+print(type(iris))
+print(type(iris.data))
+# np.c_ is the numpy concatenate function
+# which is used to concat iris['data'] and iris['target'] arrays
+# for pandas column argument: concat iris['feature_names'] list
+# and string list (in this case one string); you can make this anything you'd like..
+# the original dataset would probably call this ['Species']
+data1 = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                     columns= iris['feature_names'] + ['target'])
+
+print(type(data1))
+print(data1)

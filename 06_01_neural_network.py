e584aefec0085c4e2853bb358574baf13907913e
David Vaquero <pepesan@gmail.com>
2018-05-21 10:04:10 +0200

    mejora en el ejemplo de redes mlpc

--- 06_01_neural_network.py
+++ 06_01_neural_network.py
@@ -13,7 +13,8 @@ Y=iris.target
 X_train, X_test, Y_train, Y_test= train_test_split(X,Y)
 
 #5 capas de 6 nodos cada uno repetido el entrenamiento 900 veces
-red=MLPClassifier(max_iter=900,hidden_layer_sizes=(5,6))
+red=MLPClassifier(max_iter=1600,hidden_layer_sizes=(5,6),
+                  activation="relu",solver="adam", random_state=2)
 
 ret=red.fit(X_train,Y_train)
 print(ret)
